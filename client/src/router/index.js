a4beb336e455fb23ae1e7d74799b3ad00015a896
jonah-butler <jonahbutler6@gmail.com>
2021-01-19 13:26:40 -0500

    added songs/add route to router index.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Register from '../views/Register.vue';
 import Login from '../views/Login.vue';
 import Songs from '../views/Songs.vue';
+import CreateSong from '../views/CreateSong.vue';
 
 Vue.use(VueRouter);
 
@@ -35,6 +36,11 @@ const routes = [
     path: '/songs',
     name: 'songs',
     component: Songs,
+  },
+  {
+    path: '/songs/add',
+    name: 'create',
+    component: CreateSong,
   },
 ];
 
