7df61fbfdd811a0c919f9260e3fd87e42bf5db3d
Jonah Butler <jonahbutler6@gmail.com>
2021-01-25 15:37:23 -0500

    updated Panel to handle new slot which takes accepts a button and method

--- client/src/components/Panel.vue
+++ client/src/components/Panel.vue
@@ -5,6 +5,7 @@
                 <v-toolbar-title class="white--text">
                     <h1>{{title}}</h1>
                 </v-toolbar-title>
+                <slot name="action" />
                 <v-spacer></v-spacer>
             </v-toolbar>
             <v-divider></v-divider>
