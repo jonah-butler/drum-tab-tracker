ca031dd3311625ba0ded2188be0702d5ddf59d8d
Jonah Butler <jonahbutler6@gmail.com>
2021-01-25 15:31:55 -0500

    added Song show component and view

---
+++ client/src/components/Song.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Song Show Route
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/Song.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Song />
+  </div>
+</template>
+
+<script>
+import Song from '@/components/Song.vue';
+
+export default {
+  components: {
+    Song,
+  },
+};
+</script>
+
+<style>
+
+</style>
